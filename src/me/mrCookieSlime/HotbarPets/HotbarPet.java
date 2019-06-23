3d6ae146a3e9d27744dedd227b0f9417c7601d56
CyberPatriot <CyberPatriot@cyberpatriot.me>
2019-06-23 12:56:28 -0500

    Replace § with &

--- src/me/mrCookieSlime/HotbarPets/HotbarPet.java
+++ src/me/mrCookieSlime/HotbarPets/HotbarPet.java
@@ -13,7 +13,7 @@ public class HotbarPet extends SlimefunItem {
 
     static {
         try {
-            category = new Category(new CustomItem(CustomSkull.getItem("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjIxNjY4ZWY3Y2I3OWRkOWMyMmNlM2QxZjNmNGNiNmUyNTU5ODkzYjZkZjRhNDY5NTE0ZTY2N2MxNmFhNCJ9fX0="), "§dHotbar Pets", "", "&a> Click to open"));
+            category = new Category(new CustomItem(CustomSkull.getItem("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjIxNjY4ZWY3Y2I3OWRkOWMyMmNlM2QxZjNmNGNiNmUyNTU5ODkzYjZkZjRhNDY5NTE0ZTY2N2MxNmFhNCJ9fX0="), "&dHotbar Pets", "", "&a> Click to open"));
         } catch (Exception e) {
             e.printStackTrace();
         }
