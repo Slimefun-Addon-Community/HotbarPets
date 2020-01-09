6f2f5004df5b6c80dd7f3c072c82a1a3dd5f1a94
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-01-09 23:22:19 +0100

    Nerfed Patriot Pet

--- src/main/java/io/github/thebusybiscuit/hotbarpets/pets/special/PatriotPet.java
+++ src/main/java/io/github/thebusybiscuit/hotbarpets/pets/special/PatriotPet.java
@@ -18,9 +18,9 @@ public class PatriotPet extends SimpleBasePet {
 
 	@Override
 	public void onUseItem(Player p) {
-		p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 1));
-        p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 600, 0));
-        p.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 300, 1));
+		p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 0));
+        p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 0));
+        p.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 100, 0));
         p.getWorld().playSound(p.getLocation(), Sound.ENTITY_ZOMBIE_CONVERTED_TO_DROWNED, 1.0F, 2.0F);
 	}
 	
