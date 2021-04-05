7f5f2f354021d748dd4ad39b381e17c541887cfe
Daniel Walsh <walshydev@gmail.com>
2021-04-05 12:10:06 +0100

    Fixes #32

--- src/main/java/io/github/thebusybiscuit/hotbarpets/HotbarPetsRunnable.java
+++ src/main/java/io/github/thebusybiscuit/hotbarpets/HotbarPetsRunnable.java
@@ -16,7 +16,7 @@ public class HotbarPetsRunnable implements Runnable {
     private final HotbarPet fish;
     private final HotbarPet goldenCow;
 
-    public HotbarPetsRunnable() {
+    protected HotbarPetsRunnable() {
         chicken = (HotbarPet) SlimefunItem.getByID("HOTBAR_PET_CHICKEN");
         mooshroom = (HotbarPet) SlimefunItem.getByID("HOTBAR_PET_MOOSHROOM");
         fish = (HotbarPet) SlimefunItem.getByID("HOTBAR_PET_FISH");
@@ -29,25 +29,19 @@ public class HotbarPetsRunnable implements Runnable {
             for (int i = 0; i < 9; ++i) {
                 ItemStack item = p.getInventory().getItem(i);
 
-                if (item == null || item.getType() == null || item.getType() == Material.AIR) {
+                if (item == null || item.getType() == Material.AIR) {
                     continue;
                 }
 
-                if (isPet(item, chicken)) {
-                    if (chicken.checkAndConsumeFood(p)) {
+                if (isPet(item, chicken) && chicken.checkAndConsumeFood(p)) {
                         p.getInventory().addItem(new ItemStack(Material.EGG));
                         p.getWorld().playSound(p.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1.0F, 2.0F);
-                    }
-                } else if (isPet(item, mooshroom)) {
-                    if (mooshroom.checkAndConsumeFood(p)) {
-                        p.getInventory().addItem(new ItemStack(Material.MUSHROOM_STEM));
+                } else if (isPet(item, mooshroom) && mooshroom.checkAndConsumeFood(p)) {
+                        p.getInventory().addItem(new ItemStack(Material.MUSHROOM_STEW));
                         p.getWorld().playSound(p.getLocation(), Sound.ENTITY_COW_AMBIENT, 1.0F, 2.0F);
-                    }
-                } else if (isPet(item, fish)) {
-                    if (fish.checkAndConsumeFood(p)) {
+                } else if (isPet(item, fish) && fish.checkAndConsumeFood(p)) {
                         p.getInventory().addItem(new ItemStack(Material.COOKED_COD));
                         p.getWorld().playSound(p.getLocation(), Sound.BLOCK_WATER_AMBIENT, 1.0F, 2.0F);
-                    }
                 } else if (isPet(item, goldenCow) && goldenCow.checkAndConsumeFood(p)) {
                     p.getInventory().addItem(new ItemStack(Material.GOLD_INGOT));
                     p.getWorld().playSound(p.getLocation(), Sound.ENTITY_COW_AMBIENT, 0.8F, 2.0F);
